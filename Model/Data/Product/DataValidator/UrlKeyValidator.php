2f210851513179399b1a76f9fc639844d585b88a
James Ambardnishvili <james.ambardani@gmail.com>
2023-03-08 01:44:43 +0400

    KPIM-27 Final Changes

--- Model/Data/Product/DataValidator/UrlKeyValidator.php
+++ Model/Data/Product/DataValidator/UrlKeyValidator.php
@@ -4,50 +4,73 @@ declare(strict_types=1);
 
 namespace Itonomy\Katanapim\Model\Data\Product\DataValidator;
 
-use Magento\Catalog\Model\ResourceModel\Product;
-use Magento\UrlRewrite\Model\Storage\DbStorage;
+use Magento\Catalog\Model\Product\Url;
 
 class UrlKeyValidator implements ValidatorInterface
 {
     /**
-     * @var Product
+     * Url key attribute code
      */
-    private Product $productResource;
+    public const URL_KEY = 'url_key';
 
     /**
-     * @var DbStorage
+     * Column product sku.
      */
-    private DbStorage $storage;
+    public const COL_SKU = 'sku';
 
     /**
-     * @param Product $productResource
-     * @param DbStorage $storage
+     * @var Url
      */
-    public function __construct(
-        Product $productResource,
-        DbStorage $storage
-    ) {
-        $this->productResource = $productResource;
-        $this->storage = $storage;
+    private Url $productUrl;
+
+    /**
+     * @param Url $productUrl
+     */
+    public function __construct(Url $productUrl)
+    {
+        $this->productUrl = $productUrl;
     }
 
     /**
+     * Check if url_key exists from API or generate one from SKU, if still not available skip the row
+     *
      * @param array $productData
      * @return bool
      */
     public function validate(array &$productData): bool
     {
-        $sku = $productData['sku'];
-        $productId = $this->productResource->getProductsIdsBySkus([$sku]);
-        $rewrite = $this->storage->findOneByData(
-            ['entity_type' => 'product', 'entity_id' => $productId, 'store_id' => $productData['_store']]
-        );
+        $urlKey = $productData['url_key'];
+        if (!$urlKey) {
+            $urlKey = $this->getUrlKey($productData);
+        }
 
-        if ($rewrite) {
+        if ($urlKey) {
+            $productData['url_key'] = $urlKey;
+        } else {
             return false;
         }
 
-        unset($productData['url_key']);
         return true;
+    }
+
+    /**
+     * Retrieve url key from provided product data.
+     *
+     * @param array $productData
+     * @return string
+     */
+    protected function getUrlKey(array $productData): string
+    {
+        if (!empty($productData[self::URL_KEY])) {
+            $urlKey = (string) $productData[self::URL_KEY];
+            return $this->productUrl->formatUrlKey($urlKey);
+        }
+
+        if (!empty($productData[self::COL_SKU])
+            && (array_key_exists(self::URL_KEY, $productData))) {
+            return $this->productUrl->formatUrlKey($productData[self::COL_SKU]);
+        }
+
+        return '';
     }
 }
